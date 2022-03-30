9469a1216e2a5a9a9557bd180fa7095606643bf1
Parker Yang <99832581+ParkerYang1@users.noreply.github.com>
2022-03-30 02:47:05 +0000

    done

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(300, 300);
   }
 
   /** 
@@ -16,21 +16,49 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 255, 255);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+ 
+//quad 1
+    for (int lineX = height/20; lineX <= height/2; lineX += height/20) { 
+      line(lineX, 0, lineX, height/2);
+      line(0, lineX, height/2, lineX);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+//quad 2
+    for(int circleX = height/2 + height/12; circleX <= (height/2) + ((height/12) * 5); circleX += height/12){
+      stroke(0);
+      fill(255, 0, 255);
+      for(int x = 1; x < 6; x++){
+      ellipse(circleX, (height/12) * x, height/20, height/20);
+      }
+    }
+
+//quad 3
+  int black = 0;
+  for(int gradient = 0; gradient <= height/2; gradient++){
+    stroke(black);
+    line(gradient, height, gradient, (height/2));
+
+    black = black + 2;
+    }
+    
+//quad 4
+  stroke(0);
+  translate(height/1.3f, width/1.3f);
   
-  // define other methods down here.
-}
+  for(int petals = 0; petals <= 8; petals+=1){
+    rotate(radians(45));
+    fill(255, 176, 216);
+    ellipse(0, 0, height/24, width/4);
+    fill(145, 64, 105);
+    ellipse(0, 0, height/10, width/10);
+
+  }
+  }
+  }
+
+  
+
